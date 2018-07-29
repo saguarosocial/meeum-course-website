c06625f78256e87a6c0cda4291f7e8920f0667cc
Al <keadymore@gmail.com>
2018-07-29 13:19:47 +1000

    Installed JS to make navigation menu open and close

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,2 +1,17 @@
 // call css vars function
 cssVars();
+
+
+//open and close the nav
+
+$('.nav-prompt').click(function() {
+
+    if ($('.nav-inner').attr('aria-expanded') == 'false') {
+        $('.nav-inner').attr('aria-expanded', true);
+
+    } else {
+
+        $('.nav-inner').attr('aria-expanded', false);
+    }
+
+});
